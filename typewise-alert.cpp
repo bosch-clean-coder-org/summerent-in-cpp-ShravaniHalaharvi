7132f6dd9c023d53aba876e8767f1cdcaeb16803
ShravaniHalaharvi <136677615+ShravaniHalaharvi@users.noreply.github.com>
2023-06-21 16:58:46 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -49,8 +49,10 @@ void sendToEmail(BreachType breachType) {
   const char* recepient = "a.b@c.com";
   //Replaced Switch case with if else statements
   if(breachType == TOO_LOW)
+    printf("To: %s\n", recepient);
     printf("Hi, the temperature is too low\n");
   else if(breachType == TOO_HIGH)
+    printf("To: %s\n", recepient);
     printf("Hi, the temperature is too high\n");
 }
 
