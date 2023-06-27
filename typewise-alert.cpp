efa92b3fc424f6e0e0941b0a8e6aa092c1523542
ShravaniHalaharvi <136677615+ShravaniHalaharvi@users.noreply.github.com>
2023-06-27 19:06:38 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -20,7 +20,7 @@ BreachType classifyTemperatureBreach(
       upperLimit = 35;
   else if(coolingType == HI_ACTIVE_COOLING)
       upperLimit = 45;
-  else
+  else if(coolingType == MED_ACTIVE_COOLING)
       upperLimit = 40;
 
  return inferBreach(temperatureInC, lowerLimit, upperLimit);
